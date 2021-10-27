e07d59d8c448947030dcf9c18430b8152666ae9a
ekball <90882418+ekball@users.noreply.github.com>
2021-10-26 21:56:21 -0400

    added prompts for each type of character

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,56 @@
 // Assignment code here
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
 var specialChar = ["!", "#", "$", "%", "&", "*", "+", "-", ".", "/", ";", ":", "<", ">", "=", "?", "@", "^", "_", "~"];
-
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+
+var passLength = window.prompt("How many characters would you like to have in your password? Please choose between 8 and 128 characters.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.prompt("Would you like lowercase characters in your password?");
+ 
+window.prompt("Would you like uppercase characters in your password?");
+  
+window.prompt("Would you like special characters in your password?");
+  
+window.prompt("Would you like numbers in your password?");
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
